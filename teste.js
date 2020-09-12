16cabea05ade2d9a369180246a6d77df05c8c6e4
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2020-09-11 23:26:09 -0300

    Up teste.

--- teste.js
+++ teste.js
@@ -1,26 +1,23 @@
 const puppeteer = require('puppeteer');
 
 
-    async function Login(email,senha){
+    async function Login(page,email,senha){
         await page.waitForXPath('/html/body/div[8]/div/div/div[2]/div[5]/form/div[2]/input')
         await page.waitForTimeout(200)
-        await page.type('input[name="emailAddress"]',email)
-        await page.type('input[name="password"',senha,{delay:100})
+        await page.type('input[name="emailAddress"]',email,{delay:70})
+        await page.type('input[name="password"',senha,{delay:70})
         await page.keyboard.press('Enter')
         await page.waitForNavigation('load')
     }
 
-    async function Comprar(page,size){
+    async function Buy(page,size){
         await page.evaluate(()=>{window.scrollTo(0,1400)})
-        await page.waitForXPath('/html/body/main/div/div[1]/div[3]/div/div[2]/div[4]/div/div[2]/button[1]')
-        console.log('Sizes renderizados')
-        //await page.evaluate(()=>{document.querySelector(`label[for=tamanho__id${size}]`).click()})
-        console.log('Size Definido')
-        await page.evaluate(()=>{document.querySelector('.btn-comprar').click()})
-        console.log('Inserindo no Carrinho')
-        await page.waitForNavigation()
-        console.log('Produto no Carrinho')
+        await page.waitFor(500)
+        await page.evaluate((size)=>{document.querySelector(`label[for=tamanho__id${size}]`).click()},size)
+        await page.waitFor(500)
+        await page.evaluate(()=>{document.querySelector('#btn-comprar').click()})
     }
+
 
 async function run( model, size , email , senha ){
 
@@ -32,8 +29,9 @@ async function run( model, size , email , senha ){
         }
     )
 
-    /*const page_login = await browser.newPage()
+    const page_login = await browser.newPage()
 
+    // Login
     await page_login.goto(`https://www.nike.com.br/Snkrs`)
     page_login.waitForNavigation({waitUntil:'networkidle0'})
 
@@ -41,11 +39,15 @@ async function run( model, size , email , senha ){
         document.getElementById('anchor-acessar').click()
         console.log('Login Aberto')
     })
+    await Login(page_login,email,senha)
 
-    await Login(page_login,email,senha)*/
+    await page_login.waitForNavigation('load')
+
+    // Comprar
     const page_produto = await browser.newPage()
     await page_produto.goto(`https://www.nike.com.br/Snkrs/Produto/${model}`)
-    await Comprar(page_produto,size)
+    await Buy(page_produto,size)
+    
 }
 
 run('ISPA-OverReact-FlyKnit/153-169-211-260419','42','guirox.wd@gmail.com','Lask1234')
