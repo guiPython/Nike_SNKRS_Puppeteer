6026df180285e48f81902d843eb6d3f2d1fef339
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2020-09-11 22:02:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const puppeteer = require('puppeteer');
 
-async function Login(page,email,senha){
-    await page.waitForXPath('/html/body/div[8]/div/div/div[2]/div[5]/form/div[2]/input')
-    await page.waitForTimeout(200)
-    await page.type('input[name="emailAddress"]',email,{delay:100})
-    await page.type('input[name="password"',senha,{delay:100})
-    await page.keyboard.press('Enter')
-    await page.waitForNavigation('load')
-}
 
-async function Comprar(page,size){
-    await page.mouse.down()
-    await page.waitForXPath(`//label[contains(text(),'${size}')]`,{
-        visible:true
-    })
-    console.log('Sizes renderizados')
-    await page.click(`#tamanho__id${size}`)
-    console.log('Size Definido')
-    await page.click('#btn-comprar')
-    console.log('Inserindo no Carrinho')
-    await page.waitForNavigation()
-    console.log('Produto no Carrinho')
-}
+    async function Login(email,senha){
+        await page.waitForXPath('/html/body/div[8]/div/div/div[2]/div[5]/form/div[2]/input')
+        await page.waitForTimeout(200)
+        await page.type('input[name="emailAddress"]',email)
+        await page.type('input[name="password"',senha,{delay:100})
+        await page.keyboard.press('Enter')
+        await page.waitForNavigation('load')
+    }
+
+    async function Comprar(page,size){
+        await page.evaluate(()=>{window.scrollTo(0,1200)})
+        await page.waitForXPath('/html/body/main/div/div[1]/div[3]/div/div[2]/div[4]/div/div[2]/button[1]')
+        console.log('Sizes renderizados')
+        //await page.evaluate(()=>{document.querySelector(`label[for=tamanho__id${size}]`).click()})
+        console.log('Size Definido')
+        await page.evaluate(()=>{document.querySelector('.btn-comprar').click()})
+        console.log('Inserindo no Carrinho')
+        await page.waitForNavigation()
+        console.log('Produto no Carrinho')
+    }
 
 async function run( model, size , email , senha ){
+
+
     const browser = await puppeteer.launch(
         {
             ignoreHTTPSErrors: true,
@@ -31,7 +32,7 @@ async function run( model, size , email , senha ){
         }
     )
 
-    const page_login = await browser.newPage()
+    /*const page_login = await browser.newPage()
 
     await page_login.goto(`https://www.nike.com.br/Snkrs`)
     page_login.waitForNavigation({waitUntil:'networkidle0'})
@@ -41,10 +42,9 @@ async function run( model, size , email , senha ){
         console.log('Login Aberto')
     })
 
-    await Login(page_login,email,senha)
+    await Login(page_login,email,senha)*/
     const page_produto = await browser.newPage()
     await page_produto.goto(`https://www.nike.com.br/Snkrs/Produto/${model}`)
-    await page_produto.waitForNavigation({waitUntil:'load'})
     await Comprar(page_produto,size)
 }
 
