a5750545307e3bd7fe492a85ea72c71753a03855
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2020-09-12 00:06:24 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -4,17 +4,17 @@ const puppeteer = require('puppeteer');
     async function Login(page,email,senha){
         await page.waitForXPath('/html/body/div[8]/div/div/div[2]/div[5]/form/div[2]/input')
         await page.waitForTimeout(200)
-        await page.type('input[name="emailAddress"]',email,{delay:70})
-        await page.type('input[name="password"',senha,{delay:70})
+        await page.type('input[name="emailAddress"]',email,{delay:110})
+        await page.type('input[name="password"',senha,{delay:20})
         await page.keyboard.press('Enter')
         await page.waitForNavigation('load')
     }
 
     async function Buy(page,size){
         await page.evaluate(()=>{window.scrollTo(0,1400)})
-        await page.waitFor(500)
+        await page.waitForTimeout(500)
         await page.evaluate((size)=>{document.querySelector(`label[for=tamanho__id${size}]`).click()},size)
-        await page.waitFor(500)
+        await page.waitForTimeout(500)
         await page.evaluate(()=>{document.querySelector('#btn-comprar').click()})
     }
 
@@ -41,11 +41,10 @@ async function run( model, size , email , senha ){
     })
     await Login(page_login,email,senha)
 
-    await page_login.waitForNavigation('load')
-
     // Comprar
     const page_produto = await browser.newPage()
     await page_produto.goto(`https://www.nike.com.br/Snkrs/Produto/${model}`)
+    await page_produto.waitForTimeout(500)
     await Buy(page_produto,size)
     
 }
