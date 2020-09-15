f539ae648dcc784815bfaeea8e938a72135d06b2
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2020-09-15 19:38:22 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,53 +1,47 @@
 const puppeteer = require('puppeteer');
+const cron = require('node-cron')
 
-
-    async function Login(page,email,senha){
-        await page.waitForXPath('/html/body/div[8]/div/div/div[2]/div[5]/form/div[2]/input')
-        await page.waitForTimeout(200)
-        await page.type('input[name="emailAddress"]',email,{delay:110})
-        await page.type('input[name="password"',senha,{delay:20})
-        await page.keyboard.press('Enter')
-        await page.waitForNavigation('load')
-    }
-
-    async function Buy(page,size){
-        await page.evaluate(()=>{window.scrollTo(0,1400)})
-        await page.waitForTimeout(500)
-        await page.evaluate((size)=>{document.querySelector(`label[for=tamanho__id${size}]`).click()},size)
-        await page.waitForTimeout(500)
-        await page.evaluate(()=>{document.querySelector('#btn-comprar').click()})
-    }
-
-
-async function run( model, size , email , senha ){
-
-
-    const browser = await puppeteer.launch(
-        {
-            ignoreHTTPSErrors: true,
-            headless: false
-        }
-    )
-
-    const page_login = await browser.newPage()
-
-    // Login
-    await page_login.goto(`https://www.nike.com.br/Snkrs`)
-    page_login.waitForNavigation({waitUntil:'networkidle0'})
-
-    await page_login.evaluate(()=>{
-        document.getElementById('anchor-acessar').click()
-        console.log('Login Aberto')
-    })
-    await Login(page_login,email,senha)
-
-    // Comprar
-    const page_produto = await browser.newPage()
-    await page_produto.goto(`https://www.nike.com.br/Snkrs/Produto/${model}`)
-    await page_produto.waitForTimeout(500)
-    await Buy(page_produto,size)
-    
+async function login( email , senha , page ){
+    await page.click('#anchor-acessar')
+    await page.waitForTimeout(500)
+    await page.type('input[name="emailAddress"]',email,{delay:100})
+    await page.waitForTimeout(200)
+    await page.type('input[name="password"',senha,{delay:200})
+    await page.screenshot({path:'login_page.png'})
+    await page.keyboard.press('Enter')
 }
 
-run('ISPA-OverReact-FlyKnit/153-169-211-260419','42','guirox.wd@gmail.com','Lask1234')
+async function set_product( product , size , page ){
+    await page.goto(`https://www.nike.com.br/Snkrs/Produto/${product}`)
+    await page.waitForXPath('/html/body/main/div/div[1]/div[3]/div/div[2]/div[4]/div/div[2]/button')
+    await page.waitForTimeout(200)
+    await page.evaluate((size)=>{document.querySelector(`label[for=tamanho__id${size}]`).click()} , size )
+    await page.screenshot({path:'set_product.png'})
+    await page.click('#btn-comprar')
+}
 
+async function buy_product( page ){
+    await page.click('#carrinho > div.ckt__bg-full > div > div:nth-child(4) > a')
+    await page.waitForTimeout(300)
+    await page.click('#seguir-pagamento')
+    await page.waitForTimeout(300)
+    await page.screenshot({path:'confirm_address.png'})
+}
+
+async function run () {
+    const browser = await puppeteer.launch({headless:false})
+    const page = await browser.newPage()
+    await page.setViewport({ width:1280 , height:720})
+    await page.goto('https://www.nike.com.br/Snkrs')
+    await page.screenshot({path: 'home_page.png'})
+
+    await login('guirox.wd@gmail.com','Lask1234' , page)
+    cron.schedule('37 19 * * *', async () => {
+        await set_product('DBreak-Type/153-169-211-256298' , '42' , page);
+        await buy_product(page)
+    })
+    
+
+} 
+
+run()
