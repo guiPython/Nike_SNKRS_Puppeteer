c90b7a31265445a8e1700b47febf541809d43251
Guilherme Rocha <67191432+guiPython@users.noreply.github.com>
2020-09-15 22:36:03 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -20,28 +20,42 @@ async function set_product( product , size , page ){
     await page.click('#btn-comprar')
 }
 
-async function buy_product( page ){
-    await page.click('#carrinho > div.ckt__bg-full > div > div:nth-child(4) > a')
-    await page.waitForTimeout(300)
-    await page.click('#seguir-pagamento')
-    await page.waitForTimeout(300)
-    await page.screenshot({path:'confirm_address.png'})
+async function confirm_address( page ){
+    // TODO confirmar envio
 }
 
 async function run () {
+
     const browser = await puppeteer.launch({headless:false})
     const page = await browser.newPage()
+    page.setDefaultNavigationTimeout(0)
     await page.setViewport({ width:1280 , height:720})
+
+
     await page.goto('https://www.nike.com.br/Snkrs')
     await page.screenshot({path: 'home_page.png'})
 
-    await login('guirox.wd@gmail.com','Lask1234' , page)
-    cron.schedule('37 19 * * *', async () => {
-        await set_product('DBreak-Type/153-169-211-256298' , '42' , page);
-        await buy_product(page)
-    })
-    
 
+    await login('guirox.wd@gmail.com','Lask1234' , page)
+    cron.schedule('32 22 * * *', async () => {
+        await set_product('DBreak-Type/153-169-211-256298' , '42' , page);
+
+        // Iniciar Compra
+        await page.waitForNavigation({waitUntil:'domcontentloaded'})
+        await page.click('#carrinho > div.ckt__bg-full > div > div:nth-child(4) > a')
+
+        // Confirmar Endereco 
+        await page.waitForNavigation({waitUntil:'networkidle0'})
+        await page.click('#seguir-pagamento')
+        await page.waitForNavigation({waitUntil:'networkidle0'})
+
+        // Confirmar envio
+        // TODO modal confirmar envio
+        await page.waitForNavigation({waitUntil:'networkidle0'})
+        await page.screenshot({path:'confirm_address.png'})
+
+
+    })
 } 
 
 run()
